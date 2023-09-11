e1e4ab1ee9d4177a00de7bee6693c3d9627d1077
yebin <dev.devin.kr@gmail.com>
2023-09-12 01:59:20 +0900

    Feat: 브라우저 저장소에 저장할 dto에 memberId 추가 #3

--- src/main/java/kb04/team02/web/mvc/dto/LoginMemberDto.java
+++ src/main/java/kb04/team02/web/mvc/dto/LoginMemberDto.java
@@ -4,6 +4,7 @@ package kb04.team02.web.mvc.dto;
  * 세션 스토리지에 저장할 객체
  */
 public class LoginMemberDto {
+    private Long memberId;
     private String id;
     private String name;
     private String bankAccount;
