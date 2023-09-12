77e79365b285ec7bd224e122e0b2aacd78dfdc1d
aacara <budgielover@naver.com>
2023-09-12 14:30:53 +0900

    모임지갑 탭서비스 인터페이스 수정 및 관련 DTO 작성 #15

--- src/main/java/kb04/team02/web/mvc/dto/RuleDto.java
+++ src/main/java/kb04/team02/web/mvc/dto/RuleDto.java
@@ -1,4 +1,20 @@
 package kb04.team02.web.mvc.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class RuleDto {
+    private Long ruleId;
+    private String ruleName;
+    private Long duePrice;
+    private LocalDate dueDate;
+
 }
