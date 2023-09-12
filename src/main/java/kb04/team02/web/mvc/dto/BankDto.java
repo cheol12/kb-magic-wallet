3f752669c6020c1c2c1c107d8c18e28c6d9e9478
{cyzlcyzl} <{cyzlcyzl09@gmail.com}>
2023-09-12 13:37:22 +0900

    BankDto 추가 #18

--- src/main/java/kb04/team02/web/mvc/dto/BankDto.java
+++ src/main/java/kb04/team02/web/mvc/dto/BankDto.java
@@ -1,4 +1,18 @@
 package kb04.team02.web.mvc.dto;
 
+import kb04.team02.web.mvc.domain.member.Address;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class BankDto {
+    private Long bankId;
+    private String name;
+    private Address address;
+
 }
--- src/main/java/kb04/team02/web/mvc/service/exchange/ExchangeServiceImpl.java
+++ src/main/java/kb04/team02/web/mvc/service/exchange/ExchangeServiceImpl.java
@@ -1,10 +1,41 @@
 package kb04.team02.web.mvc.service.exchange;
 
+import kb04.team02.web.mvc.dto.BankDto;
+import kb04.team02.web.mvc.dto.OfflineReceiptDto;
+import kb04.team02.web.mvc.dto.WalletDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 @Service
-public class ExchangeServiceImpl{
+public class ExchangeServiceImpl implements ExchangeService{
 
+    @Override
+    public List<BankDto> bankList() {
+        return null;
+    }
 
+    @Override
+    public List<WalletDto> chairManWalletList() {
+        return null;
+    }
 
+    @Override
+    public Long selectedWalletBalance() {
+        return null;
+    }
+
+    @Override
+    public List<OfflineReceiptDto> offlineReceiptHistory() {
+        return null;
+    }
+
+    @Override
+    public int requestOfflineReceipt() {
+        return 0;
+    }
+
+    @Override
+    public List<OfflineReceiptDto> cancelOfflineReceipt() {
+        return null;
+    }
 }
