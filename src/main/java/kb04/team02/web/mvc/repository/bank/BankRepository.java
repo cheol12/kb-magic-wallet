6b480cc96927e8b73134d39ee06fa11130a52ba6
{cyzlcyzl} <{cyzlcyzl09@gmail.com}>
2023-09-12 09:24:02 +0900

    ExchangeInterface

--- src/main/java/kb04/team02/web/mvc/repository/bank/BankRepository.java
+++ src/main/java/kb04/team02/web/mvc/repository/bank/BankRepository.java
@@ -1,4 +1,18 @@
 package kb04.team02.web.mvc.repository.bank;
 
-public interface BankRepository {
+import kb04.team02.web.mvc.domain.bank.Bank;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BankRepository extends JpaRepository<Bank, Long> {
+
+    /*
+    ROWNUM 43
+
+    SQL
+    SELECT * FROM BANK
+
+    JPA: BankRepository.findAll()
+    */
 }
