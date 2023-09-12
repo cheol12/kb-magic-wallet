e02b6a2ca3d628514aa6d344ddee4b253a1dd348
{cyzlcyzl} <{cyzlcyzl09@gmail.com}>
2023-09-12 14:34:45 +0900

    exchangeService - bankList 완료 #18

--- src/main/java/kb04/team02/web/mvc/service/exchange/ExchangeServiceImpl.java
+++ src/main/java/kb04/team02/web/mvc/service/exchange/ExchangeServiceImpl.java
@@ -3,15 +3,26 @@ package kb04.team02.web.mvc.service.exchange;
 import kb04.team02.web.mvc.dto.BankDto;
 import kb04.team02.web.mvc.dto.OfflineReceiptDto;
 import kb04.team02.web.mvc.dto.WalletDto;
+import kb04.team02.web.mvc.repository.bank.BankRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class ExchangeServiceImpl implements ExchangeService{
+
+    private final BankRepository bankRepository;
 
     @Override
     public List<BankDto> bankList() {
-        return null;
+        List<BankDto> bankList = bankRepository.findAll().stream()
+                .map(BankDto::toBankDto)
+                .collect(Collectors.toList());
+
+        return bankList;
     }
 
     @Override
