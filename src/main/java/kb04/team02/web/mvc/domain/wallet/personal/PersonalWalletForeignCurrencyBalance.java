091bc9e3f3cf7f6a476773f64b3c890f3209d55b
{cyzlcyzl} <{cyzlcyzl09@gmail.com}>
2023-09-14 10:38:14 +0900

    conflict edit 2

--- src/main/java/kb04/team02/web/mvc/domain/wallet/personal/PersonalWalletForeignCurrencyBalance.java
+++ src/main/java/kb04/team02/web/mvc/domain/wallet/personal/PersonalWalletForeignCurrencyBalance.java
@@ -5,7 +5,6 @@ import kb04.team02.web.mvc.domain.wallet.group.GroupWallet;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.DiscriminatorValue;
@@ -18,7 +17,6 @@ import javax.persistence.ManyToOne;
 @AllArgsConstructor
 @Getter
 @DiscriminatorValue("PersonalWalletForeign")
-@Setter
 public class PersonalWalletForeignCurrencyBalance extends ForeignCurrencyBalance{
     //== 연관관계 설정 START==//
     @ManyToOne
