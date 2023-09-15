f1d2ec31bfec3ebfac9a9bf0c71e1a3ab86511ab
yebin <dev.devin.kr@gmail.com>
2023-09-15 10:48:07 +0900

    Feat: LoggingAspect #82
     - 모든 controller 의 methods 를 포인트컷으로 등록
     - 전달된 파리미터 출력. 단 제대로 출력하려면 toString() 필요.

--- src/main/java/kb04/team02/web/mvc/common/aop/LoggingAspect.java
+++ src/main/java/kb04/team02/web/mvc/common/aop/LoggingAspect.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Aspect
 @Component
 public class LoggingAspect {
@@ -18,6 +21,15 @@ public class LoggingAspect {
     public void logBefore(JoinPoint joinPoint) {
         // 메소드 실행 전에 출력 되는 로그
         logger.info("Before executing: " + joinPoint.getSignature());
+
+        // 전달된 파라미터 출력. toString() 필요.
+        Object[] methodArgs = joinPoint.getArgs();
+        if (methodArgs != null && methodArgs.length > 0) {
+            logger.info("Method arguments:");
+            for (Object arg : methodArgs) {
+                logger.info("  " + arg.toString());
+            }
+        }
     }
 
     @After("execution(* kb04.team02.web.mvc.*.controller.*.*(..))")
