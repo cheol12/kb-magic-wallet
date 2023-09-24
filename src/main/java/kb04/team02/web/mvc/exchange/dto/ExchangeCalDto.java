5520d7ba6ca254f2def65a684d03d77ffe3796af
{cyzlcyzl} <{cyzlcyzl09@gmail.com}>
2023-09-24 14:28:11 +0900

    exchangeController Swagger 적용 중 #235

--- src/main/java/kb04/team02/web/mvc/exchange/dto/ExchangeCalDto.java
+++ src/main/java/kb04/team02/web/mvc/exchange/dto/ExchangeCalDto.java
@@ -1,5 +1,6 @@
 package kb04.team02.web.mvc.exchange.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,10 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ExchangeCalDto {
     // 계산 금액, 현재 고시 환율, 적용 환율
+    @ApiModelProperty(example = "환율 적용 금액")
     private Long expectedAmount;
+    @ApiModelProperty(example = "현재 고시 환율")
     private Double tradingBaseRate;
+    @ApiModelProperty(example = "적용 환율")
     private Double applicableExchangeRate;
 }
